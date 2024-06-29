81912acadd45f0cff82128f33f92d4c24e3ab197
priyanjal7 <154314100+priyanjal7@users.noreply.github.com>
2024-06-29 08:52:30 +0530

    Made simple calculator using react js

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calculator from './Components/Calculator'
+
+const App = () => {
+  return (
+      <div className='w-full h-screen bg-zinc-900 flex items-center justify-center'>
+          <Calculator/>
+      </div>
+  )
+}
+
+export default App
